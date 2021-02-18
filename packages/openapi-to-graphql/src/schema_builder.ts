442308ab9e8d4d516297a9e845c26bc2181c3b28
Nils Lundquist <nils@bitovi.com>
2021-02-18 11:20:41 -0700

    fix typo
    Signed-off-by: Nils Lundquist <nils@bitovi.com>

--- packages/openapi-to-graphql/src/schema_builder.ts
+++ packages/openapi-to-graphql/src/schema_builder.ts
@@ -664,7 +664,7 @@ function createFields<TSource, TContext, TArgs extends object>({
       fetch
     })
 
-    const nullableProperty = typeof def.schema.nullable
+    const nullableProperty = def.schema.nullable
 
     // Finally, add the object type to the fields (using sanitized field name)
     if (objectType) {
